c056bb292335ff6d808ee9de9d77b97785b852ff
Daniel Castro <dancassig@gmail.com>
2018-11-18 14:50:05 -0600

    Django now uses docker postgres image as database

--- {{cookiecutter.project_slug}}/config/settings.py
+++ {{cookiecutter.project_slug}}/config/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{{cookiecutter.project_slug}}',
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': '',
     }
 }
 
