49a2ae4d191f4c27af1457ede1ac487507b47926
Daniel Castro <dancassig@gmail.com>
2018-11-26 13:56:00 -0600

    Added latex_elements config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -113,21 +113,10 @@ htmlhelp_basename = 'DjangoCookiecutterdoc'
 # -- Options for LaTeX output ------------------------------------------------
 
 latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+    'papersize': 'letterpaper',
+    'pointsize': '10pt',
+    'preamble': '',
+    'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
