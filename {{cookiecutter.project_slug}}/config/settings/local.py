f98107098d4eceaedcad42b0a3f363455cbd2ba2
Daniel Castro <dancassig@gmail.com>
2018-11-27 08:02:08 -0600

    Added beat_schedule to local for testing purposes

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -9,3 +9,8 @@ INSTALLED_APPS += ['django_extensions']
 
 INSTALLED_APPS += ['{{cookiecutter.project_slug}}.functional_tests']
 INSTALLED_APPS += ['{{cookiecutter.project_slug}}.test_app']
+
+CELERY_BEAT_SCHEDULE['test-task1'] = {
+    'task': '{{cookiecutter.project_slug}}.test_app.tasks.task1',
+    'schedule': 10,
+}
