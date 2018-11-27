dfc321bbb2498b6076d6abc05884a025b4dcbfa1
Daniel Castro <dancassig@gmail.com>
2018-11-27 16:32:00 -0600

    Added migrations to production and new tempalte_context

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django_admin_env_notice.context_processors.from_settings",
             ],
         },
     },
