ab495b7ef97e16eb59107f7478cd2bbd9c97e4e6
Daniel Castro <dancassig@gmail.com>
2018-12-11 13:23:43 -0600

    Registered user model to admin

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from {{ cookiecutter.project_slug }}.users.forms import UserChangeForm, UserCreationForm
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+    fieldsets = auth_admin.UserAdmin.fieldsets
+    list_display = ["username", "first_name", "last_name", "is_superuser", "is_staff"]
+    search_fields = ["username"]
