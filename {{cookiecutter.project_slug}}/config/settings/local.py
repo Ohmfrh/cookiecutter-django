7805b021a6f99b039eea39c87fab874e8b05c4ab
Daniel Castro <dancassig@gmail.com>
2018-12-19 12:55:29 -0600

    Updated settings layout with comments

--- {{cookiecutter.project_slug}}/config/settings/local.py
+++ {{cookiecutter.project_slug}}/config/settings/local.py
@@ -1,16 +1,34 @@
 from .base import *
 
-
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='!!!SET DJANGO_SECRET_KEY!!!')
-
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='!!!SET DJANGO_SECRET_KEY!!!')
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+]
 
-ALLOWED_HOSTS = ['*']
-
+# django-extensions
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ['django_extensions']
 
+# FUNCTIONAL TESTS
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ['{{cookiecutter.project_slug}}.functional_tests']
-INSTALLED_APPS += ['{{cookiecutter.project_slug}}.test_app']
+
+# Celery
+# ------------------------------------------------------------------------------
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-always-eager
+CELERY_TASK_ALWAYS_EAGER = True
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-eager-propagates
+CELERY_TASK_EAGER_PROPAGATES = True
 
 CELERY_BEAT_SCHEDULE['test-task1'] = {
     'task': '{{cookiecutter.project_slug}}.test_app.tasks.task1',
@@ -22,5 +40,14 @@ CELERY_BEAT_SCHEDULE['test-task2'] = {
     'schedule': 60,
 }
 
+# django_admin_env_notice
+# ------------------------------------------------------------------------------
 ENVIRONMENT_NAME = "Development"
 ENVIRONMENT_COLOR = "#8E8E8E"
+
+# TEST APP
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += ['{{cookiecutter.project_slug}}.test_app']
+
+# Your stuff...
+# ------------------------------------------------------------------------------
--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -1,13 +1,17 @@
 from .base import *
 
-
+# GENERAL
+# ------------------------------------------------------------------------------
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='!!!SET DJANGO_SECRET_KEY!!!')
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['{{cookiecutter.domain_name}}']
 
-STATIC_ROOT = 'static_prod'
-
+# django_admin_env_notice
+# ------------------------------------------------------------------------------
 ENVIRONMENT_NAME = "Production server"
 ENVIRONMENT_COLOR = "#FF2222"
+
+# Your stuff...
+# ------------------------------------------------------------------------------
