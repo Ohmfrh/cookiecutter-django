b994393866fd587f92cef91103532f94b10c6837
Daniel Castro <dancassig@gmail.com>
2019-01-15 19:12:24 -0600

    Added hosts with deployment script

--- {{cookiecutter.project_slug}}/deployment/delpoy.sh
+++ {{cookiecutter.project_slug}}/deployment/delpoy.sh
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+(ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ./deploy/deploy.yml \
+    -u {{cookiecutter.remote_server_username}} \
+    -i ./deployment_hosts \
+    --private-key={{cookiecutter.server_connection_key_path}})
